8a325ca5bc539023ad977d84f17b513538a10f62
ASS-NIANG <assniang@ASS-NIANG.localdomain>
2022-11-23 10:26:22 +0000

    awx: some updates

--- awx_scripts/awx_ui_build.sh
+++ awx_scripts/awx_ui_build.sh
@@ -5,4 +5,5 @@ cd /home/ansible-awx/
 
 gnome-terminal --tab -- sh -c "sudo make docker-compose; bash"
 
-gnome-terminal --tab -- sh -c "sudo sleep 100 && sudo docker exec tools_awx_1 make clean-ui ui-devel && xdg-open https://localhost:8043/#/home; bash"
+gnome-terminal --tab -- sh -c "sleep 100 && sudo docker exec tools_awx_1 make clean-ui ui-devel && xdg-open https://localhost:8043/#/home; bash"
+gnome-terminal --tab -- sh -c "sleep 100 && sudo docker exec -ti tools_awx_1 awx-manage createsuperuser"
