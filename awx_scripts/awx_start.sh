6afcfd73899d67283d7d45569f5327043a7635de
ASS-NIANG <assniang@ASS-NIANG.localdomain>
2022-11-23 09:40:03 +0000

    awx: some updates

--- awx_scripts/awx_start.sh
+++ awx_scripts/awx_start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo rm awx_ui_build.sh && sleep 3
-
 cd /home/ansible-awx/
 
 gnome-terminal --tab --title=DockerCompose -- sh -c "sudo make docker-compose"
--- awx_scripts/awx_ui_build.sh
+++ awx_scripts/awx_ui_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+gnome-terminal --tab -- sh -c "rm Desktop/awx_ui_build.sh"
 
 cd /home/ansible-awx/
 
-gnome-terminal --tab -- sh -c  "sudo sleep 100 && sudo docker exec tools_awx_1 make clean-ui ui-devel && xdg-open https://localhost:8043/#/home"
+gnome-terminal --tab -- sh -c "sudo make docker-compose; bash"
 
-gnome-terminal --tab -- sh -c  "sudo make docker-compose"
-
+gnome-terminal --tab -- sh -c "sudo sleep 100 && sudo docker exec tools_awx_1 make clean-ui ui-devel && xdg-open https://localhost:8043/#/home; bash"
